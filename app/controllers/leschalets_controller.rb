e65825fb00fba299288775c8b0190b9384fd7485
Emma Colombat <emma.colombat69@gmail.com>
2020-11-29 14:35:22 +0100

    created redirection and controller

---
+++ app/controllers/leschalets_controller.rb
@@ -0,0 +1,5 @@
+class LeschaletsController < ApplicationController
+  def index
+
+  end
+end
---
+++ test/controllers/leschalets_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LeschaletsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
